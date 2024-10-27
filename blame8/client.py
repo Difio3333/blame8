4dd06bd0797a3d0476d394adb3e7607b37d1272d
Fridolin <fridolin.kerner@gmx.de>
2024-10-27 16:08:59 +0100

    first blaming commit

--- blame8/client.py
+++ blame8/client.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     # in order to be able to count if a singular 
-
+    # heloo
 
     # here is a list of the hooks we are using. Key is name of the hook, value is the command with which the terminal calls it, I think
     # the --edits make it so they don't actually change anyhting but just check and report errors
