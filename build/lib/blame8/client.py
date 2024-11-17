aa01b2acfb35c74f1afe265ef9233218396e4827
Fridolin <fridolin.kerner@gmx.de>
2024-11-17 17:01:03 +0100

    testing verbosity

--- build/lib/blame8/client.py
+++ build/lib/blame8/client.py
@@ -3,8 +3,8 @@ import sys
 
 
 def main():
-    # in order to be able to count if a singular 
-    # heloo
+   print("Testing verbosity")
+   print("Double verbose")
 
     # here is a list of the hooks we are using. Key is name of the hook, value is the command with which the terminal calls it, I think
     # the --edits make it so they don't actually change anyhting but just check and report errors
@@ -12,7 +12,7 @@ def main():
     hooks = {
         "isort": "isort . --check-only",
         "flake8": "flake8 . --check",
-        "black": "black . --ignore=E501"
+        "black": "black . --ignore=E501",
     }
 
     # would be cool to make this so it draws from .pre-commit-config.yaml dynamically
@@ -35,9 +35,8 @@ def main():
         #leads to "Failed" output in prehook
 
 def run_hook(hook_name, command) -> bool:
-    verbose = False
+    verbose = True
 
-   
     if "isort" in hook_name:
         pass
 
@@ -52,10 +51,10 @@ def run_hook(hook_name, command) -> bool:
         # make it so it's only talking when doing the integrated pre-commit hooks.
 
     try:
-        subprocess.run(command, shell=True, check=True, capture_output=True)
+        result = subprocess.run(command, shell=True, check=True, capture_output=True)
         if verbose:
             print(f"{hook_name} passed!")
-        return True
+        return result.returncode == 0
         # this could just say return True I think but I'm too afraid to change it.
         #returncode = 0 is succes and 1 is failure.
 
